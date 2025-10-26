d6349b5fcdcf23bf0ea2a1c226ceae2832c0b2a8
Zhen'ya <125275567+Yainra@users.noreply.github.com>
2025-10-26 21:31:33 +0100

    Did a thing in preparation to hopefully do more things later

---
+++ scraps/main.py
@@ -0,0 +1,30 @@
+# === imports ===
+
+# from question_table import questions
+# from utils.score_tracker import update_score, display_score
+
+
+
+# === core functions ===
+
+# === load_questions() ===
+# - load, and shuffle the question list.
+
+
+
+# === ask_questions() ===
+# - display a question, take user input, check answer and return result.
+
+
+
+# === run_quiz() ===
+# - main quiz loop, run through all questions (and track score?)
+
+
+
+# === main execution ===
+# - entry point of the script.
+
+
+
+# === Run Script ===
